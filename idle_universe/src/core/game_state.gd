ba6a517e4837ba49b4673573312c61b115a482de
Wholesale <tonycj2015@gmail.com>
2026-03-24 21:26:02 -0400

    added plugin

--- idle_universe/src/core/game_state.gd
+++ idle_universe/src/core/game_state.gd
@@ -1,1 +1,26 @@
 extends RefCounted
+
+class_name GameState
+
+var dust: DigitMaster
+var elements : Dictionary
+var upgrades : Dictionary
+var current_element_id : String
+var next_unlock_id : String
+var world_level : int
+var global_multiplier : DigitMaster
+var tick_count : int
+var total_played_seconds : float
+var last_save_tick : int
+
+func _init() -> void:
+	dust = DigitMaster.new(0.0)
+	elements = {}
+	upgrades = {}
+	current_element_id = "ele_P"
+	next_unlock_id = "ele_H"
+	world_level = 0
+	global_multiplier = DigitMaster.new(1.0, 0)
+	tick_count = 0
+	total_played_seconds = 0.0
+	last_save_tick = 0
